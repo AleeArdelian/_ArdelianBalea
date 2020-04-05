7803cd16af138e026b8e82bafc94df8d490f19e0
Alexandru Balea <alexandru.balea@halcyonmobile.com>
2020-04-05 19:30:33 +0300

    Added BBT EC TC

--- src/test/java/ro/ubbcluj/cs/AppTest.java
+++ src/test/java/ro/ubbcluj/cs/AppTest.java
@@ -1,92 +1,128 @@
 package ro.ubbcluj.cs;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ro.ubbcluj.cs.domain.Student;
-import ro.ubbcluj.cs.domain.Tema;
-import ro.ubbcluj.cs.repository.AbstractCRUDRepository;
-import ro.ubbcluj.cs.repository.StudentRepository;
-import ro.ubbcluj.cs.repository.TemaRepository;
+import ro.ubbcluj.cs.repository.StudentXMLRepository;
+import ro.ubbcluj.cs.service.Service;
 import ro.ubbcluj.cs.validation.StudentValidator;
-import ro.ubbcluj.cs.validation.TemaValidator;
 import ro.ubbcluj.cs.validation.Validator;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    private static final String STUDENTS_FILE = "test-studenti.xml";
+
+    private static final Validator<Student> studentValidator = new StudentValidator();
+
+    private static Service service;
+
+    @BeforeClass
+    public static void init()  {
+        final StudentXMLRepository studentRepository = new StudentXMLRepository(studentValidator, STUDENTS_FILE);
+        studentRepository.delete("aaa1111");
+        service = new Service(studentRepository, null, null);
     }
 
     @Test
-    public void saveStudent_ValidStudent_WorksFine() {
-        final String id = "123";
-        final String name = "Balea Alexandru";
-        final int group = 931;
-        final Student student = new Student(id, name, group);
+    public void addStudent_validStudent_studentIsAdded() {
+        // Given
+        final String id = "aaa1111";
+        final String nume = "Balea Alexandru";
+        final int grupa = 931;
 
-        final Validator<Student> studentValidator = new StudentValidator();
-        final AbstractCRUDRepository<String, Student> studentRepository = new StudentRepository(studentValidator);
+        // When
+        final int result = service.saveStudent(id, nume, grupa);
 
-        final Student savedStudent = studentRepository.save(student);
-        final Student savedStudent1 = studentRepository.save(student);
-
-        assertNull(savedStudent);
-        assertEquals(savedStudent1, student);
+        // Then
+        assertEquals(result, 1);
     }
 
     @Test
-    public void saveStudent_InvalidStudent_ThrowsValidatorException() {
+    public void addStudent_emptyId_studentNotValid() {
+        // Given
         final String id = "";
-        final String name = "Balea Alexandru";
-        final int group = 931;
-        final Student student = new Student(id, name, group);
+        final String nume = "Balea Alexandru";
+        final int grupa = 931;
 
-        final Validator<Student> studentValidator = new StudentValidator();
-        final AbstractCRUDRepository<String, Student> studentRepository = new StudentRepository(studentValidator);
+        // When
+        final int result = service.saveStudent(id, nume, grupa);
 
-        final Student savedStudent = studentRepository.save(student);
-
-        assertNull(savedStudent);
+        // Then
+        assertEquals(result, 0);
     }
 
     @Test
-    public void addAssignment_ValidAssignment_WorksFine() {
-        final String id = "12";
-        final String descriere = "Tema 1";
-        final int deadline = 13;
-        final int startline = 11;
-        final Tema tema = new Tema(id, descriere, deadline, startline);
+    public void addStudent_idAlreadyExists_studentNotAdded() {
+        // Given
+        final String id = "aaa1111";
+        final String nume = "Ardelian Alexandra";
+        final int grupa = 931;
 
-        final Validator<Tema> temaValidator = new TemaValidator();
-        final AbstractCRUDRepository<String, Tema> temaRepository = new TemaRepository(temaValidator);
+        // When
+        final int result = service.saveStudent(id, nume, grupa);
 
-        final Tema savedTema = temaRepository.save(tema);
-
-        assertNull(savedTema);
+        // Then
+        assertEquals(result, 0);
     }
 
     @Test
-    public void addAssignment_InvalidAssignment_ThrowsValidatorException() {
-        final String id = "12";
-        final String descriere = "Tema 1";
-        final int deadline = 11;
-        final int startline = 14;
-        final Tema tema = new Tema(id, descriere, deadline, startline);
+    public void addStudent_emptyName_studentNotValid() {
+        // Given
+        final String id = "aaa1112";
+        final String nume = "";
+        final int grupa = 931;
 
-        final Validator<Tema> temaValidator = new TemaValidator();
-        final AbstractCRUDRepository<String, Tema> temaRepository = new TemaRepository(temaValidator);
+        // When
+        final int result = service.saveStudent(id, nume, grupa);
 
-        final Tema savedTema = temaRepository.save(tema);
-
-        assertNull(savedTema);
+        // Then
+        assertEquals(result, 0);
     }
+
+    @Test
+    public void addStudent_nameWithNonAlphaCharacters_studentNotValid() {
+        // Given
+        final String id = "aaa1113";
+        final String nume = "Balea Alexandru 121312312";
+        final int grupa = 931;
+
+        // When
+        final int result = service.saveStudent(id, nume, grupa);
+
+        // Then
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void addStudent_groupNotPositive_studentNotValid() {
+        // Given
+        final String id = "aaa1114";
+        final String nume = "Balea Alexandru";
+        final int grupa = -431;
+
+        // When
+        final int result = service.saveStudent(id, nume, grupa);
+
+        // Then
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void addStudent_groupLengthNot3_studentNotValid() {
+        // Given
+        final String id = "aaa1115";
+        final String nume = "Balea Alexandru";
+        final int grupa = 931123;
+
+        // When
+        final int result = service.saveStudent(id, nume, grupa);
+
+        // Then
+        assertEquals(result, 0);
+    }
+
 }
